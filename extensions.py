97d264eab82e5d5c8fb2dff3104e58e68c2751dd
Shpately <130307155+Shpately@users.noreply.github.com>
2023-05-16 22:48:22 +0300

    extensions.py

--- extensions.py
+++ extensions.py
@@ -1,16 +1,16 @@
 import requests
 import json
 
-keys = {'доллар' : 'USD',
-        'евро' : 'EUR',
-        'рубль' : 'RUR'}
+keys = {'доллар': 'USD',
+        'евро': 'EUR',
+        'рубль': 'RUB'}
 
 payload = {}
-headers= {
-        "apikey": "ipT969LyS8V3ywjZGmnsUgDEvEPMIgwU"
-        }
-    
-        
+headers = {
+    "apikey": "ipT969LyS8V3ywjZGmnsUgDEvEPMIgwU"
+}
+
+
 class APIException(Exception):
     pass
 
@@ -18,19 +18,33 @@ class APIException(Exception):
 class Converter:
     def __init__(self):
         pass
-   
-    @staticmethod
-    def get_price(base, quote, amount):
-        if base, quote in keys:
-                url = f"https://api.apilayer.com/exchangerates_data/convert?to={keys[base]}&from={keys[quote]}&amount={amount}"
-                response = requests.request("GET", url, headers=headers, data = payload)
 
-                if response.status_code == 200:
-                    result = json.loads[response.content]
-                    return result
-                    # raise ValueError(f"Invalid response format from API: {data}")
-                else:
-                    raise ConnectionError(f"Failed to fetch data from API: {response.status_code}")
+    @staticmethod
+    def get_price(base, quote, amount, bot, message):
+        base_true, quote_true = None, None
+        try:
+            base_true = keys[base]
+        except KeyError:
+            bot.send_message(message.chat.id, 'Валюта введена неправильно')
+            # raise APIException("Валюта введена неправильно")
+
+        try:
+            quote_true = keys[quote]
+        except KeyError:
+            bot.send_message(message.chat.id, 'Валюта введена неправильно')
+            # raise APIException("Валюта введена неправильно")
+
+        # url = f'https://api.apilayer.com/currency_data/convert?base={base}&symbols={quote}&amount={amount}&date=2023-05-16'
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={quote_true}&from={base_true}&amount={amount}"
+        response = requests.get(url, headers=headers)  # , data=payload)  # request("GET", url, headers=headers, data=payload)
+
+        if response.status_code == 200:
+            if base_true == quote_true:
+                raise APIException("Нельзя конвертировать одну и ту же валюту")
+
+            result = json.loads(response.content)['result']
+            return result
+            # raise ValueError(f"Invalid response format from API: {data}")
         else:
-            return "Введите корректные данные"
-           
+            bot.send_message(message.chat.id, f"Взаимодействие с API невозможно: {response.status_code}")
+            # raise ConnectionError(f"Взаимодействие с API невозможно: {response.status_code}")
