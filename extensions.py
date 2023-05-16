98e297306062d335e26347defade506056a97aa9
Shpately <130307155+Shpately@users.noreply.github.com>
2023-05-16 12:52:43 +0300

    extensions.py

--- extensions.py
+++ extensions.py
@@ -1,26 +1,33 @@
 import requests
+import json
 
+keys = {'доллар' : 'USD',
+        'евро' : 'EUR',
+        'рубль' : 'RUR'}
 
+payload = {}
+headers= {
+        "apikey": "ipT969LyS8V3ywjZGmnsUgDEvEPMIgwU"
+        }
+    
+    
 class Converter:
-    def __init__(self, base, quote, amount, url):
-        self.base = base
-        self.quote = quote
-        self.amount = amount
-        self.url = url
+    def __init__(self):
+        pass
 
     @staticmethod
-    def get_price():
-        def get_price(base, quote, amount):
-            url = f"https://api.example.com/rates?base={base}&quote={quote}"
-            response = requests.get(url)
-
-            if response.status_code == 200:
-                data = response.json()
-                if 'price' in data:
-                    price = data['price']
-                    converted_amount = amount * price
-                    return converted_amount
-                else:
-                    raise ValueError(f"Invalid response format from API: {data}")
+    def get_price(base, quote, amount):
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={keys[base]}&from={keys[quote]}&amount={amount}"
+        response = requests.request("GET", url, headers=headers, data = payload)
+        
+        if response.status_code == 200:
+            result = json.loads[response.content]
+            data = response.json()
+            if 'price' in data:
+                price = data['price']
+                converted_amount = amount * price
+                return converted_amount
             else:
-                raise ConnectionError(f"Failed to fetch data from API: {response.status_code}")
+                raise ValueError(f"Invalid response format from API: {data}")
+        else:
+            raise ConnectionError(f"Failed to fetch data from API: {response.status_code}")
